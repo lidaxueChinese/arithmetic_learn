4a061c2c66e382de4f355afcc964582171160c0c
lidaxue <lidaxue@bitnei.cn>
2020-06-18 20:09:50 +0800

    修改packet 名

---
+++ src/main/java/arithmetic/aa202006/partition/CommonSubStr.java
@@ -0,0 +1,45 @@
+package arithmetic.aa202006.partition;
+/**
+ * Created by lidaxue on 2020/6/18.
+ * 最长公共子串
+ */
+public class CommonSubStr {
+
+    public static void main(String[] args) {
+        String str1 = "asd";
+        String str2 = "sd";
+        int value = getCommonStrLength(str1,str2);
+        System.out.println("value is:"+value);
+    }
+
+
+    private static int getCommonStrLength(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                dp[i][j] = 0;
+            }
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (dp[i][j] > max) {
+                    max = dp[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+}
